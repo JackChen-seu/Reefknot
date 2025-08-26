a62801a6657f35decbba33be8fadc3661b5219bd
xuhaodong <hxu541@connect.hkust-gz.edu.cn>
2025-08-26 15:23:29 +0800

    update dir_name

--- infer.sh
+++ infer.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=2
 python LLaVA/infer_LLaVA_yesandno.py \
     --model-path /hpc2hdd/home/yuxuanzhao/lijungang/nk_code/LLM/liuhaotian/llava-v1.5-13b \
     --question-file Dataset/YESNO.jsonl \
-    --answers-file Reefknot/YesNo_results.jsonl \
+    --answers-file Result/YesNo_results.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
     --apha 0.1 \
